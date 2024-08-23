88d0f77f15e3ad5e0bb2350d6f2717aae465aa57
Anate Muhammed <anatemuhammed@gmail.com>
2024-08-23 19:42:44 +0100

    second commit

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
+// //
+// setInterval(() => {
+//   console.log("Hello! World");
+// }, 2000);
+// console.log("I will run the first task");
 const http = require("http");
-const port = 7000;
-const app = http.createServer((req, res) => {
-  // console.log(req);
-
-  if (req.url === "/") {
-    res.write("Hello! Welcome to Home Page");
-    res.end();
-  }
-  if (req.url === "/profile") {
-    res.write("Welcome to Profile Page");
-    res.end();
-  }
-  if (req.url === "/about-us") {
-    res.write("Here is what you have to known about-us");
-    res.end();
-  }
-  res.write(`<h1>Oops!</h1>
-    <p>Page Not Found </p>
-    <small>Sorry! you can try some other time
-    <a href="/">Back Home</a>`);
+const port = 5000;
+const server = http.createServer((req, res) => {
+  console.log("requset event");
+  res.end("Hello! World");
 });
 
-app.listen(port, () => {
-  console.log("Example app is Listening on ", port);
+server.listen(port, () => {
+  console.log("Example app listen on port:", port);
 });
